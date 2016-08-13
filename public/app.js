fb6e8c736a46821885d4d5e0e93bd2a72d3907fc
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-08-12 23:32:58 -0400

    Added heroku files

--- public/app.js
+++ public/app.js
@@ -19,8 +19,10 @@ function getNews(){
 	$('#stories').empty();
 	$.getJSON('/news', function(data){
 		for(var i = 0; i < data.length; i++){
-			$('#stories').prepend('<tr><td>' + data[i].title + 
-				'</td><td>' + data[i].story + '</td><button class="addcomment" data-id="' + 
+			$('#stories').prepend(
+				'<tr><td>' + data[i].title + '</td>' + 
+				'<td>' + data[i].story + '</td>' + 
+				'<td><button class="addcomment" data-id="' + 
 				data[i]._id + '">Add Comment</button></td></tr>');
 		}
 		$('#stories').prepend('<tr><th>Title</th><th>Story</th>');
